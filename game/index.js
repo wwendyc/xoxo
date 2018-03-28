0f230b1463e8dbdbbb13b060cc2e91ce87f9675f
wwendyc <wendy.changw@gmail.com>
2018-03-28 12:29:09 -0400

    milestone one

--- game/index.js
+++ game/index.js
@@ -1,6 +1,22 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+let board = Map()
+const initialState = {board: board, turn: 'X'}
+
+export const MOVE = 'MOVE'
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case MOVE:
+      let turn = state.turn
+      const board = state.board.setIn(action.payload.position, turn)
+      turn === 'X' ? turn = 'O' : turn = 'X'
+      return {board, turn}
+    default:
+      return state
+  }
+}
+
+export const move = (player, position) => {
+  return {type: MOVE, payload: {position, player}}
 }
